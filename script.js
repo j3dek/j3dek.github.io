0fbab67f98c04d1d5eafe4a896952002dc1c41b6
j3dek <158775170+j3dek@users.noreply.github.com>
2024-03-09 22:48:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.getElementById('dysk').addEventListener('click', function(e) {
-    e.preventDefault(); // Zapobiegamy domyślnej akcji kliknięcia na link
-    var password = prompt('Wprowadź hasło:'); // Wyświetlamy pole do wprowadzania hasła
-    if (password === '4444') { // Sprawdzamy, czy hasło jest poprawne
-        window.location.href = 'dysk.html'; // Przekierowujemy użytkownika do docelowego linku
-    } else {
-        alert('Nieprawidłowe hasło.'); // Wyświetlamy alert, jeśli hasło jest nieprawidłowe
-    }
-});
+
