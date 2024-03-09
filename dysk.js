01b87cc566c03da1ae73f6bad7795fa693b2839d
j3dek <158775170+j3dek@users.noreply.github.com>
2024-03-09 22:41:26 +0000

    test

---
+++ dysk.js
@@ -0,0 +1,17 @@
+async function uploadFile() {
+    const fileInput = document.getElementById('file_input');
+    const file = fileInput.files[0];
+    let formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'ibuydrxa'); // Zastąp 'YOUR_UPLOAD_PRESET' swoim ustawieniem wstępnym Cloudinary
+  
+    try {
+      const response = await axios.post('https://api.cloudinary.com/v1_1/dpdnurge6/upload', formData, { // Zastąp 'YOUR_CLOUD_NAME' nazwą swojej chmury Cloudinary
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      document.getElementById('status').innerHTML = `Plik został przesłany! URL: ${response.data.url}`;
+    } catch (error) {
+      console.error(error);
+      document.getElementById('status').innerHTML = 'Błąd podczas przesyłania pliku.';
+    }
+  }
